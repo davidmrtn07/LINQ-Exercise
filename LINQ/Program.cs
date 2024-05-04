ce5bac74ca4109aa26636942625750c6d43eb4e3
davidmrtn07 <davidmrtn07@gmail.com>
2024-05-04 09:33:21 -0500

    Initial Save Part 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
 {
-    public class Program
+    public class MainClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            new Program();
         }
+        public class Program
+        {
+            List<string> designerBrands = new List<string>()
+        {
+            "Nike",
+            "Adidas",
+            "Ralph Lauren",
+            "Versace"
+        };
+
+            public Program()
+            {
+                designerBrands.Add("Levi");
+
+                var orderedBrands = designerBrands.OrderBy(name => name.Length);
+                foreach (var brand in orderedBrands)
+                {
+                    Console.WriteLine(brand);
+                }
+            }
+        }
+
     }
+        
 }
